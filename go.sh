60135bb010e37d8c97d966151ecf62b37d82fca9
John Chee <cheecheeo@gmail.com>
2014-10-14 18:53:50 -0700

    update go.sh

--- go.sh
+++ go.sh
@@ -1,1 +1,2 @@
 cabal sandbox add-source ../thrift/lib/hs/
+cabal exec ghc Main.hs gen-hs/ItemStore_Client.hs gen-hs/ItemStore.hs gen-hs/ItemStore_Iface.hs gen-hs/Performance_Consts.hs gen-hs/Performance_Types.hs -- -Wall -O2
